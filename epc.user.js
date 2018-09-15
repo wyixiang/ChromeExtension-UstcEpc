6729f6df7baf74b742bd7dccdd40be9e2f1e9881
Unknown <1103366093@qq.com>
2018-09-15 11:14:27 +0800

    0.1.0
    1.这是初代版本，参考了学长zuoyuan的项目，实现了我需求的最基本功能。  
    2.与学长的项目类似，插件分为两个主要js脚本，分别做配置功能和预约选课功能。  
    3.config页面尽量做的简单易用，相比学长的项目删去了一些无用的配置信息。  
    4.预约选课功能简单写了一下，面向的主要需求是捡漏，不过对后续开发应该还算友好，反正就算项目里有shit也多半是我自己吃，大不了就重构嘛。  
    5.系统写了一下文档，以后可能不会这么勤快了。

--- epc.user.js
+++ epc.user.js
@@ -1,54 +1,18 @@
-﻿// ==UserScript==  
-// @name         epc
-// @version              1.0
-// @author       zuoyuan@mail.ustc.edu.cn
-// @namespace    https://github.com/zuoyuan
-// @description  epc 刷课
-// @include      http://epc.ustc.edu.cn/m_practice.asp*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
-// ==/UserScript== 
-
+﻿// @author       yixiangw@mail.ustc.edu.cn
+// @url    https://github.com/wyixiang
 
 //scroll to end
-/**尾部*/
 window.scrollTo(0,10000);
 	
-/*	
-var jqueryUrl = 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
-var script = document.createElement('script');
-script.src = jqueryUrl;
-script.type = 'text/javascript';
-script.onload = function(){
-	main();
-}
 
-document.body.appendChild(script);
-*/
-var DAYTIME = {
-day:['周一','周二','周三','周四','周五'],
-time:['08:25-09:15','09:45-11:25','14:00-15:30','14:30-16:10','16:40-17:30','19:00-20:40']
-};
 var Config=function(){
 	this.conf = {};
 	if(typeof(localStorage['adaytime'])=='undefined') localStorage['adaytime']='';
 	this.conf.adaytime = localStorage['adaytime'].split(',');
-	if(typeof(localStorage['DAYTIME'])=='undefined') localStorage['DAYTIME']='';
-	DAYTIME = localStorage['DAYTIME'].split(',');
 	if(typeof(this.conf.adaytime)=='undefined'){
 		this.conf.adaytime=[];
-		for(var i=0;i<DAYTIME.day.length;i++){
-			for(var j=0;j<DAYTIME.time.length;j++){
-				this.conf.adaytime.push ( DAYTIME.day[i]+' '+DAYTIME.time[j]);
-			}
-		}
-		
 	}
-	//console.log(this.adaytime);
-	this.conf.username = localStorage['username'];
-	this.conf.pw = localStorage['pw'];
-	if(typeof(this.conf.username)=='undefined' || typeof(this.conf.pw)=='undefined'){
-		console.log('username or password empty.');
-	}
+	//console.log(this.conf.adaytime);
 	
 	this.conf.startWeek = typeof(localStorage['startWeek'])=='undefined'? 1:localStorage['startWeek'];
 	this.conf.endWeek = typeof(localStorage['endWeek'])=='undefined'? 30:localStorage['endWeek'];
@@ -62,25 +26,17 @@ Config.prototype.set = function(data){
 	}
 }
 var config = new Config();
-var allPage ;
-var c = config.get();
+var configlist = config.get();
+
 function main(){
-	
-	
-	/**  自动登录  */
+	/*  登录提示  */
 	var pageText = $('table table table:last tr:last').text();
 	if(pageText=="登录后可以查看详细信息"){
-		$.post('n_left.asp','submit_type=user_login&name='+c.username+'&pass='+c.pw+'&txt_check=&user_type=2&Submit=LOG+IN',function(data){
-			console.log('Log in OK!');
-			location.reload();
-		});
+		alert('你没有登录');
 		return ;
 	}
-	
-	
-	var pageInfo = pageText.match(/\d+/g);
-	var cpage = pageInfo[0];
-	allPage = pageInfo[2];
+	//console.log('1111');
+	//console.log(c.adaytime);
 	
 	var rootTable = $('table table table table:eq(0)');
 	var course = [];
@@ -95,10 +51,6 @@ function main(){
 		obj.hour = $(this).find('td:eq(4)').text();
 		obj.time = $(this).find('td:eq(5)').html().toString();
 		obj.time = obj.time.substring(obj.time.search('>')+1);
-		if(DAYTIME.indexOf(obj.time)==-1){
-			DAYTIME.push(obj.time);
-			localStorage['DAYTIME'] = DAYTIME;
-		}
 		
 		obj.size = parseInt($(this).find('td:eq(9)').text());
 		obj.students = parseInt($(this).find('td:eq(10)').text());
@@ -110,31 +62,19 @@ function main(){
 	
 	for(var i=0;i<course.length;i++){
 		var courseWeek = parseInt(course[i].week.match(/\d+/)[0]);
-		if(courseWeek < c.startWeek || courseWeek > c.endWeek) {
-			if(0 == i){
-				var url = location.href;
-				if(url.indexOf('week=')==-1){
-					url += '&week='+c.startWeek;
-				}
-				if(url.indexOf('week_day=')==-1){
-					url += '&week_day=1';
-				}
-				if(url.indexOf('page=')==-1){
-					url += '&page=1';
-				}
-				location.href = url.replace(/week=\d+/,'week='+c.startWeek).replace(/week_day=\d/,'week_day=1').replace(/page=\d+/,'page=1').replace(/\&isall.+/,'');
-			}
+		//周数不符
+		if(courseWeek < configlist.startWeek || courseWeek > configlist.endWeek) {
 			count++;
 			continue;
 		}
-		
-		if(c.adaytime.indexOf(course[i].day+' '+course[i].time)==-1){
+		//星期&时间不符
+		if(configlist.adaytime.indexOf(course[i].day+' '+course[i].time)==-1){
 			count++;
 			continue;
 		}
+		//课程无法选择
 		if(course[i].btn.find(':submit').is(':disabled') || course[i].btn.find(':submit').val()!='预 约'
-			|| course[i].btn.text().trim()=="已达选课上线"){
-		//if(course[i].size == course[i].students)
+			|| course[i].btn.text().trim()=="已达选课上限"){
 			count++;
 			continue;
 		}
@@ -165,31 +105,13 @@ function main(){
 }
 
 $(document).bind('next',function(){
-	var d = new Date(localStorage['firstWeek']).getTime();
-	var cd = new Date().getTime();
-	var cweek = Math.floor(1 + (cd-d)/3600000/24/7);
-	var cday = new Date().getDay()+1;//从第二天开始刷课
-	var week = location.href.match(/week=(\d+)/);
-	var day = location.href.match(/week_day=(\d+)/);
-	var url = location.href;
-	if(week==null){
-		url = url + '&week='+cweek;
-		week=['week='+cweek];
-	}
-	if(day==null){
-		url = url + '&week_day='+cday;
-		day=['week_day='+cday];
-	}
-	var page = location.href.match(/page=(\d+)/);
-	if(page==null){
-		url = url + '&page=1';
-		page=['page=1',1];
-	}
 	var T = 5000;
-	url = 'http://epc.ustc.edu.cn/' + $('a[title="下一页"]').attr('href').trim();
-		setTimeout(function(){
-			location.href=url;
-		},T);
+	url = 'http://epc.ustc.edu.cn/' + $('a[title="上一页"]').attr('href').trim();
+	//console.log('333');
+	setTimeout(function(){
+		//console.log('222');
+		location.href=url;
+	},T);
 	
 });
 
--- epcSetup.js
+++ epcSetup.js
@@ -7,15 +7,11 @@ var week=['周一','周二','周三','周四','周五','周六','周日'];
 var timeSeg = 
 {
 	'Situational Dialogue':['08:25-09:15','16:40-17:30'],
-	'Topical Discussion':['09:45-11:25','14:30-16:10','19:00-20:40']
+	'Topical Discussion':['09:45-11:25','14:30-16:10','19:00-20:40'],
+	'Drama':['19:00-20:40']
 };
 var html = '<style>#time li li{display:inline-block;}</style><ul id="time" style="list-style:none;">';
-html += '<li>'
-			+'<h3>User Info</h3>'
-				+'<ul>'
-					+'<li>ID<input type="text" id="username" value="'+localStorage['username']+'"/> Password<input type="password" id="pw"  value="'+localStorage['pw']+'"/></li>'
-				+'</ul>'
-			+'</li>';
+
 if(typeof(localStorage['adaytime'])=='undefined'){
 	var adaytime=[];
 }else{
@@ -35,17 +31,15 @@ for(var i in timeSeg){
 	}
 	html += '</ul></li>';
 }
+
 html += '<li><h3>Start and End week</h3>'
-	 + 'one day of first week<input id="first-week" type="date" value="' +localStorage['firstWeek'] + '">'
 	 + 'from week<input id="start-week" type="number" value="'+localStorage['startWeek']+'"> to week<input id="end-week" type="number" value="'+localStorage['endWeek']+'"></li>';
 html += '<li><input id="save" type="button" value="Save"></li>';
 html += '</ul>';
 rootNode.html(html);
+
 $('#save').click(function(e){
 	var dateTime=[];
-	localStorage['username']=$('#username').val();
-	localStorage['pw']=$('#pw').val();
-	localStorage['firstWeek']=$('#first-week').val();
 	localStorage['startWeek']=$('#start-week').val();
 	localStorage['endWeek']=$('#end-week').val();
 	$('#time').children('li.adaytime').each(function(){
